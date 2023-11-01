b3da6f618b3354a637c1eadcaf232e8243f446d5
Igor Rizhyi <igorrizhyi@gmail.com>
2023-11-01 17:14:21 +0200

    add UA layout

--- config.h
+++ config.h
@@ -32,26 +32,6 @@
 
 #define UNICODE_SELECTED_MODES UNICODE_MODE_MACOS
 
-enum unicode_names {
-    DE_LOWER,
-    DE_UPPER,
-    EF_LOWER,
-    EF_UPPER,
-};
-
-const uint32_t PROGMEM unicode_map[] = {
-    [YI_LOWER] = 0x0439,
-    [YI_UPPER] = 0x0419,
-    [CC_LOWER] = 0x0446,
-    [CC_UPPER] = 0x0426,
-    [UU_LOWER] = 0x0443,
-    [UU_UPPER] = 0x0423,
-    [KA_LOWER] = 0x043A,
-    [KA_UPPER] = 0x041A,
-    [EE_LOWER] = 0x0435,
-    [EE_UPPER] = 0x0415,
-};
-
 #define XP_YI XP(YI_LOWER, YI_UPPER)
 #define XP_CC XP(CC_LOWER, CC_UPPER)
 #define XP_UU XP(UU_LOWER, UU_UPPER)
---
+++ keymap.c
@@ -0,0 +1,23 @@
+// Copyright 2022 Google LLC.
+// SPDX-License-Identifier: Apache-2.0
+
+enum unicode_names {
+    DE_LOWER,
+    DE_UPPER,
+    EF_LOWER,
+    EF_UPPER,
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [YI_LOWER] = 0x0439,
+    [YI_UPPER] = 0x0419,
+    [CC_LOWER] = 0x0446,
+    [CC_UPPER] = 0x0426,
+    [UU_LOWER] = 0x0443,
+    [UU_UPPER] = 0x0423,
+    [KA_LOWER] = 0x043A,
+    [KA_UPPER] = 0x041A,
+    [EE_LOWER] = 0x0435,
+    [EE_UPPER] = 0x0415,
+};
+
