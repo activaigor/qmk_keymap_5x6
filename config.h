74bedfde4aa00438cf82ca527639bf2e0f12d1b4
Igor Rizhyi <igorrizhyi@gmail.com>
2023-11-01 16:47:52 +0200

    add UA layout

--- config.h
+++ config.h
@@ -39,24 +39,16 @@ enum unicode_names {
     EF_UPPER,
 };
 
-// й ц у к е
-// ф і в а п
-// я ч с м и
-
 const uint32_t PROGMEM unicode_map[] = {
-    [YI_LOWER] = 0x0439,  // й
+    [YI_LOWER] = 0x0439,
     [YI_UPPER] = 0x0419,
-
-    [CC_LOWER] = 0x0446,  // ц
+    [CC_LOWER] = 0x0446,
     [CC_UPPER] = 0x0426,
-
-    [UU_LOWER] = 0x0443,  // у
+    [UU_LOWER] = 0x0443,
     [UU_UPPER] = 0x0423,
-
-    [KA_LOWER] = 0x043A,  // к
+    [KA_LOWER] = 0x043A,
     [KA_UPPER] = 0x041A,
-
-    [EE_LOWER] = 0x0435,  // е
+    [EE_LOWER] = 0x0435,
     [EE_UPPER] = 0x0415,
 };
 
