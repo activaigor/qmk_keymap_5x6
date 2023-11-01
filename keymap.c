4d34c348bdeb2334ca38da5484f693f5ce9ca325
Igor Rizhyi <igorrizhyi@gmail.com>
2023-11-01 17:15:53 +0200

    add UA layout

--- keymap.c
+++ keymap.c
@@ -8,7 +8,7 @@ enum unicode_names {
     EF_UPPER,
 };
 
-const uint32_t PROGMEM unicode_map[] = {
+const uint32_t unicode_map[] PROGMEM = {
     [YI_LOWER] = 0x0439,
     [YI_UPPER] = 0x0419,
     [CC_LOWER] = 0x0446,
