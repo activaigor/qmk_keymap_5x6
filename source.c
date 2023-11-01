11c6b60cc58b6bcfe080d137606f51c9e98fed43
Igor Rizhyi <igorrizhyi@gmail.com>
2023-11-01 19:45:58 +0200

    add keymap.c

--- source.c
+++ source.c
@@ -1,29 +1,16 @@
 #include QMK_KEYBOARD_H
 
 
-enum unicode_names {
-    YI_LOWER,
-    YI_UPPER,
-    CC_LOWER,
-    CC_UPPER,
-    UU_LOWER,
-    UU_UPPER,
-    KA_LOWER,
-    KA_UPPER,
-    EE_LOWER,
-    EE_UPPER,
-};
-
 const uint32_t PROGMEM unicode_map[] = {
-    [YI_LOWER] = 0x0439,
-    [YI_UPPER] = 0x0419,
-    [CC_LOWER] = 0x0446,
-    [CC_UPPER] = 0x0426,
-    [UU_LOWER] = 0x0443,
-    [UU_UPPER] = 0x0423,
-    [KA_LOWER] = 0x043A,
-    [KA_UPPER] = 0x041A,
-    [EE_LOWER] = 0x0435,
-    [EE_UPPER] = 0x0415,
+    0x0439,
+    0x0419,
+    0x0446,
+    0x0426,
+    0x0443,
+    0x0423,
+    0x043A,
+    0x041A,
+    0x0435,
+    0x0415,
 };
 
